5e4143837b552e9ccc933035792343030ea43a7b
kaj117 <70688258+kaj117@users.noreply.github.com>
2022-12-16 21:40:41 +0530

    Add files via upload

---
+++ helpers/display_progress.py
@@ -0,0 +1,71 @@
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+import math, os, time, shutil
+from config import Config
+from text import Translation
+
+async def progress_for_pyrogram(
+    current,
+    total,
+    ud_type,
+    message,
+    filename,
+    start
+):
+    now = time.time()
+    diff = now - start
+    if round(diff % 10.00) == 0 or current == total:
+        percentage = current * 100 / total
+        speed = current / diff
+        elapsed_time = round(diff) * 1000
+        time_to_completion = round((total - current) / speed) * 1000
+        estimated_total_time = elapsed_time + time_to_completion
+
+        elapsed_time = TimeFormatter(milliseconds=elapsed_time)
+        estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
+
+        progress = "[{0}{1}] {2}%\n".format(
+            ''.join(["●" for i in range(math.floor(percentage / 10))]),
+            ''.join(["○" for i in range(10 - math.floor(percentage / 10))]),
+            round(percentage, 2),
+            filename
+        )
+        tmp = progress + """🔺<b>𝗗𝗢𝗡𝗘</b> ✅: {0} of {1}\n🔺<b>𝗦𝗣𝗘𝗘𝗗</b> 🚀: {2}/s\n🔺<b>𝗧𝗜𝗠𝗘</b> 🕒: {3}""".format(
+            humanbytes(current),
+            humanbytes(total),
+            humanbytes(speed),
+            TimeFormatter(time_to_completion) if time_to_completion != '' else "0 s"
+        )
+        try:
+            await message.edit(
+                text="{}\n {}".format(
+                    ud_type,
+                    tmp
+                )
+            )
+        except:
+            pass
+
+def humanbytes(size):
+    if not size:
+        return ""
+    power = 2**10
+    n = 0
+    Dic_powerN = {0: ' ', 1: 'K', 2: 'M', 3: 'G', 4: 'T'}
+    while size > power:
+        size /= power
+        n += 1
+    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
+
+def TimeFormatter(milliseconds: int) -> str:
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = ((str(days) + "d, ") if days else "") + \
+        ((str(hours) + "h, ") if hours else "") + \
+        ((str(minutes) + "m, ") if minutes else "") + \
+        ((str(seconds) + "s, ") if seconds else "") + \
+        ((str(milliseconds) + "ms, ") if milliseconds else "")
+    return tmp[:-2]
